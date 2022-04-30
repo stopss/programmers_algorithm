57f10c081d9356fddba2d6c28a5ce0948b34aa2c
jieun <stopss417@gamil.com>
2022-05-01 01:08:41 +0900

    programmers

--- Level1/가운데글자가져오기/solution.py
+++ Level1/가운데글자가져오기/solution.py
@@ -1,2 +1,7 @@
-def solution(num):
-    print()
+def solution(s):
+    a = str(s)
+    if len(s) % 2 == 1:
+        answer = s[len(s)//2]
+    else:
+        answer = s[(len(s)//2)-1]+s[len(s)//2]
+    return answer
